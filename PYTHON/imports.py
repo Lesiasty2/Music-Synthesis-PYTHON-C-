2911a8cace6c1fb7d5d11069281cd55bb1844f83
Lesiasty2 <Lesiasty2@gmail.com>
2026-03-23 18:44:10 +0100

    Setting up directories, python virtual environment with libraries, .gitignore file, and first python files

---
+++ PYTHON/imports.py
@@ -0,0 +1,18 @@
+"""
+import.py - Imports all libraries and PATH dependencies
+"""
+#STL PYTHON
+import json
+import os
+from pathlib import Path
+import time
+
+#DSP PROTOTYPE
+import numpy as np
+from scipy import signal
+
+#VISUALISATION
+import matplotlib.pyplot as plt
+
+#AUDIO RESPONCE
+import sounddevice as sd
